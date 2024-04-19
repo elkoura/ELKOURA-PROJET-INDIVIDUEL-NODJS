1d81e9616fc96ed0977dac0380ed234641345038
ThomasdevLC <thomas.lecam@gmail.com>
2024-04-19 14:44:46 +0200

    index server

--- controller/barsController.js
+++ controller/barsController.js
@@ -1,2 +1,25 @@
+const Bars = require("../models/Bars");
+
 const controller = {};
-const Bar = require("../models/Bars");
+
+controller.store = (req, res) => {
+  const bar = {
+    name: req.body.name,
+    adresse: req.body.adresse,
+    tel: req.body.tel,
+    email: req.body.email,
+    price: req.body.price,
+  };
+
+  console.log(bar);
+
+  Bars.create(bar)
+    .then((bar) => {
+      res.json(bar);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+};
+
+module.exports = controller;
