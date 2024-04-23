07484c7b268f46d1a541175d4650f826a62da85d
kieransweetman <kikisweetman@gmail.com>
2024-04-23 11:56:06 +0200

    cleanup

--- controller/commandesController.js
+++ controller/commandesController.js
@@ -94,14 +94,13 @@ const CommandeController = {
             .catch((err) => res.status(500).json({ error: err.message }));
     },
     pdf: (req, res) => {
-        // - GET / commande / details /: id_commande => renvoie un pdf de la commande
         const { id_commande } = req.params;
 
         Commande.findByPk(id_commande)
             .then((commande) => {
                 const commandeData = commande.dataValues;
                 const pdfDoc = new jsPDF();
-                console.log(commandeData);
+
                 const items = [];
                 for (const key in commandeData) {
                     items.push(`${key}: ${commandeData[key]}`);
@@ -115,70 +114,5 @@ const CommandeController = {
             .catch((err) => res.status(500).json({ error: err.message }));
     }
 };
-//controller d'affichage des commandes
-// controller.getAll = (req, res) => {
-// 	//arguments de getAll():requete (req) et resultat (res)
-// 	Commande.findAll().then((queryResult) => {
-// 		// findAll() , then() = fonctions (req = findAll() 												,res=queryResult()
-// 		res.render("index", { commandes: queryResult }); // render=fonction res
-// 	});
-// };
-
-// //controller des creations de commande
-// controller.create = (req, res) => {
-// 	res.render("create");
-// };
-
-// //controller des mises a jour de commandes
-// controller.edit = (req, res) => {
-// 	Commande.findByPk(req.params.id).then((commande) => {
-// 		res.render("edit", { commandes: commande });
-// 	});
-// };
-
-// //controller d'affichage d'une commande
-// controller.store = (req, res) => {
-// 	const commande = {
-// 		name: req.body.name,
-// 		prix: req.body.prix,
-// 		bars_id: req.body.bars_id,
-// 		date: req.body.date,
-// 		status: req.body.status
-// 	};
-// };
-
-// console.log(commande);
-
-// //creation d'une commande
-// Commande.create(commande)
-// 	.then((commande) => {
-// 		res.redirect("/commandes");
-// 	})
-// 	.catch((error) => {
-// 		res.send(error);
-// 	});
-
-// //controller de mise a jour
-// controller.update = (req, res) => {
-// 	const commande = {
-// 		name: req.body.name,
-// 		prix: parseFloat(req.body.prix),
-// 		bars_id: req.body.bars_id,
-// 		date: req.body.date,
-// 		status: req.body.status
-// 	};
-// };
-
-// //mise a jour d'une commande avec id
-// Commande.update(commande, { where: { id: req.params.id } })
-// 	.then((commande) => res.redirect("/commandes"))
-// 	.catch((error) => {
-// 		res.send(error);
-// 	});
-
-// //controller de suppression d'une commande
-// controller.destroy = (req, res) => {
-// 	commande.destroy({ where: { id: req.params.id } }).then(() => res.redirect("/commandes"));
-// };
 
 module.exports = CommandeController;
