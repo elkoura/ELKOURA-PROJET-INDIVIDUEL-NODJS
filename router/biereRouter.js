e1a4a6008363a7c0b1703b0cf4a19cd9a6e02b5b
elkoura <elkourafouad@gmail.com>
2024-04-23 10:53:59 +0200

    changer

--- router/biereRouter.js
+++ router/biereRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const biereController = require("../controller/BiereController");
-
-router.post("/:id_bar/biere", validateCreation(), biereController.createBiere);
-router.put("/biere/:id_biere", validateIdBiere(), biereController.updateBiere);
-router.delete("/biere/:id_biere", validateIdBiere(), biereController.deleteBiere);
-router.get("/:id_bar/biere", validateIdBiere(), biereController.listBieres);
-router.get("/biere/:id_biere", validateIdBiere(), biereController.getBiere);
+const { validateBiere, validateIdParam, updateValidateBiere } = require("../validators/biereValidator");
+router.post("/:id_bar/biere", validateBiere(), biereController.createBiere);
+router.put("/biere/:id_biere", updateValidateBiere(), biereController.updateBiere);
+router.delete("/biere/:id_biere", validateIdParam(), biereController.deleteBiere);
+router.get("/:id_bar/biere", validateIdParam(), biereController.listBieres);
+router.get("/biere/:id_biere", validateIdParam(), biereController.getBiere);
 
 module.exports = router;
